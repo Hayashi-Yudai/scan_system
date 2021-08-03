a4474c5de18ac2e8a8520274bf62aeb912e811e2
Yudai Hayashi <34836226+Hayashi-Yudai@users.noreply.github.com>
2021-08-03 18:15:36 +0900

    Introduced Plotly.js as a test

--- core/static/core/js/plot_spectl.js
+++ core/static/core/js/plot_spectl.js
@@ -1,12 +1,22 @@
-var ctx = document.getElementById("canvas");
-
-// TODO: stream data
-let data = {
-  labels: x,
-  datasets: [{ data: y, fill: false, borderColor: "rgba(255, 0, 0, 0.5)" }],
+var trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  type: "scatter",
 };
-var myChart = new Chart(ctx, {
-  type: "line",
-  data: data,
-  // options: { animation: { duration: 0 } },
-});
+
+var trace2 = {
+  x: [1, 2, 3, 4],
+  y: [16, 5, 11, 9],
+  type: "scatter",
+};
+
+var data = [trace1, trace2];
+var layout = {
+  width: 550,
+  height: 400,
+  margin: { l: 30, r: 0, b: 3, t: 3, pad: 5 },
+  showlegend: true,
+  legend: { orientation: "h" },
+};
+
+Plotly.newPlot("coord", data, layout);
