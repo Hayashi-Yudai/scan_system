bb0447df3e0031ccd5924774c1bda0adc045143a
Yudai Hayashi <34836226+Hayashi-Yudai@users.noreply.github.com>
2021-08-04 18:19:45 +0900

    Move fft checkbox's behavior into common

--- core/static/core/js/ajax_common.js
+++ core/static/core/js/ajax_common.js
@@ -50,3 +50,55 @@ document.getElementById("save-data").addEventListener("submit", (e) => {
       console.log("error in save data");
     });
 });
+
+document
+  .querySelector("input[name=fft-checkbox]")
+  .addEventListener("change", async (e) => {
+    let url = fft_url;
+    let type = document.getElementById("fft-type").value;
+    let fftx;
+    let ffty;
+    if (e.target.checked) {
+      await fetch(url, {
+        method: "POST",
+        body: `fft=true&type=${type}`,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((responseJson) => {
+          fftx = responseJson.x;
+          ffty = responseJson.y;
+        })
+        .catch((error) => {});
+    } else {
+      await fetch(url, {
+        method: "POST",
+        body: `fft=false&type=${type}`,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((responseJson) => {
+          fftx = responseJson.x;
+          ffty = responseJson.y;
+        })
+        .catch((error) => {
+          console.log("FFT error");
+        });
+    }
+    trace1 = {
+      x: fftx,
+      y: ffty,
+      type: "scatter",
+    };
+
+    data = [trace1];
+    Plotly.newPlot("canvas", data, layout);
+  });
