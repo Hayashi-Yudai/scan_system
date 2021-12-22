095c01c22d003663c1b7a6b1645e9384fd92c3cf
Yudai Hayashi <yudai0913haya@gmail.com>
2021-12-22 12:35:58 +0900

    Fixed UI of archive page

--- archive/static/archive/js/plot.js
+++ archive/static/archive/js/plot.js
@@ -1,16 +1,13 @@
-var trace1 = {
+const trace1 = {
   x: [],
   y: [],
   type: "scatter",
 };
 
-var data = [trace1];
-var layout = {
-  width: 500,
-  height: 400,
+const data = [trace1];
+const layout = {
   margin: { l: 50, r: 0, b: 3, t: 20, pad: 5 },
-  showlegend: true,
-  showlegend: false ,
+  showlegend: false,
 };
 
 Plotly.newPlot("canvas", data, layout);
