e46f298150508f713c48a3cd1fbe69d31d259f19
Yudai Hayashi <yudai0913haya@gmail.com>
2021-12-22 12:20:30 +0900

    Fixed UI of rapid scan page

--- core/static/core/js/plot_spectl.js
+++ core/static/core/js/plot_spectl.js
@@ -9,8 +9,6 @@ const trace1 = {
 
 const data = [trace1];
 const layout = {
-  // width: 600,
-  // height: 400,
   margin: { l: 50, r: 0, b: 3, t: 20, pad: 5 },
   showlegend: true,
   legend: { orientation: "h" },
