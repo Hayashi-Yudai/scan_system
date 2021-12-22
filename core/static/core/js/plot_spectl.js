a5f7d10693e40e536c72ed5af88b80594693a892
Yudai Hayashi <yudai0913haya@gmail.com>
2021-12-22 11:27:54 +0900

    Fixed layout of the step scan page

--- core/static/core/js/plot_spectl.js
+++ core/static/core/js/plot_spectl.js
@@ -1,16 +1,16 @@
 let x;
 let y;
 
-var trace1 = {
+const trace1 = {
   x: x,
   y: y,
   type: "scatter",
 };
 
-var data = [trace1];
-var layout = {
-  width: 600,
-  height: 400,
+const data = [trace1];
+const layout = {
+  // width: 600,
+  // height: 400,
   margin: { l: 50, r: 0, b: 3, t: 20, pad: 5 },
   showlegend: true,
   legend: { orientation: "h" },
