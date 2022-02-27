9e4a05c49bf2a18536ffe25ebb65879590cc8bb4
Yudai Hayashi <yudai0913haya@gmail.com>
2022-02-28 04:01:15 +0900

    Removed unused code

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -7,11 +7,9 @@ function validateSamplingRate(event) {
   if (rate > 100 || rate < 0) {
     samplingRate.classList.toggle("invalid-value");
     document.getElementById("sampling-rate-alert").classList.remove("hide");
-    document.getElementById("set-sampling-rate").disabled = true;
   } else {
     samplingRate.classList.remove("invalid-value");
     document.getElementById("sampling-rate-alert").classList.add("hide");
-    document.getElementById("set-sampling-rate").disabled = false;
   }
 }
 
