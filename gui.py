79f3309e1587a05168c08709bf49695783b379c5
Yudai Hayashi <yudai0913haya@gmail.com>
2022-02-27 15:26:23 +0900

    Refactored gui code

--- gui.py
+++
@@ -1,5 +0,0 @@
-from webgui import FlaskUI
-from scan_system.wsgi import application
-
-if __name__ == "__main__":
-    FlaskUI(application, start_server="django", maximized=True).run()
--- run.bat
+++ run.bat
@@ -4,10 +4,6 @@ cd /d %~dp0
 set PORT=8000
 set CONDA_ENV=thz
 
-
-REM start chrome.exe http://localhost:%PORT%
-
 call activate %CONDA_ENV%
-REM call python.exe manage.py runserver %PORT%
-call python.exe gui.py
+call python.exe webgui.py
 call deactivate
