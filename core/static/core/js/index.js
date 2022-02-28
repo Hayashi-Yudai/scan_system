3d5096aae3bc01e46480bf856f340f01cb4fe077
Yudai Hayashi <yudai0913haya@gmail.com>
2022-02-28 11:56:28 +0900

    Adjusted style

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -5,10 +5,11 @@ samplingRate.addEventListener("input", validateSamplingRate);
 function validateSamplingRate(event) {
   let rate = Number(event.currentTarget.value);
   if (rate > 100 || rate < 0) {
-    samplingRate.classList.toggle("invalid-value");
+    // samplingRate.classList.toggle("invalid-value");
+    samplingRate.classList.toggle("is-error");
     document.getElementById("sampling-rate-alert").classList.remove("hide");
   } else {
-    samplingRate.classList.remove("invalid-value");
+    samplingRate.classList.remove("is-error");
     document.getElementById("sampling-rate-alert").classList.add("hide");
   }
 }
